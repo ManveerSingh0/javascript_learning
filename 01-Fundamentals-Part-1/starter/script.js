5ab4e5b47154e2f9a6cecbc9c3cea0edf81bb14d
ManveerSingh0 <singhcoder2005@gmail.com>
2024-02-18 19:21:20 +0530

    did variable and values assignment

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+//values and variables
+let js = "amazing , i can do this";
+let number = 23;
+
+//              Values and Variables assignment
+
+let country = "INDIA";
+let continent = "Asia";
+let population = "140.76";
+
+/*
+    -- ways to name variables
+    -**use camel case in javascript
+    1._variableName
+    2. $variablename
+    3. c++ (variable name)
+*/
+
+console.log(40 + 40);
+console.log(number);
+console.log(js);
