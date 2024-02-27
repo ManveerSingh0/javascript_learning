32f56d8b2f34e42fd763833b67755187a7d651fb
ManveerSingh0 <singhcoder2005@gmail.com>
2024-02-27 10:24:07 +0530

    don't know what i am doing

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -7,15 +7,31 @@ let number = 23;
 let country = "INDIA";
 let continent = "Asia";
 let population = "140.76";
+let javascriptIsFun = true;
+
+
+//              Data types
+let isIsland = false;
+let language; // or let language = undefined; 
+console.log(typeof isIsland , typeof language);
+javascriptIsFun = "YES!";
+
+//almost do same thing
+let children = undefined;
+let secondChildren = null;
+const PI = 3.1415;
+
+//you can't do this --> let something{"value"};
 
 /*
     -- ways to name variables
     -**use camel case in javascript
     1._variableName
-    2. $variablename
+    2. $variablename10
     3. c++ (variable name)
 */
 
 console.log(40 + 40);
 console.log(number);
+console.log(typeof country);
 console.log(js);
