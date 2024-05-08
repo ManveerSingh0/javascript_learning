9c5a183c8f2d01914bb780e82e632febb07f1ac6
ManveerSingh0 <singhcoder2005@gmail.com>
2024-05-08 08:53:39 +0530

    added color and 30rem font

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,7 +14,7 @@ console.log(document.querySelector('.guess').value);
 
 const secret_number = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secret_number;
-
+let score = 20;
 
 function log_into_console(){
     const guess = Number(document.querySelector('.guess').value);
@@ -25,9 +25,31 @@ function log_into_console(){
     }
     else if(guess === secret_number){
         document.querySelector('.message').textContent = "You Won!!!";
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = "30rem";
     }
     else if(guess > secret_number){
-        document.querySelector('.message').textContent = "Too High";
+        if(score > 1){
+            document.querySelector('.message').textContent = "Too High";
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+          document.querySelector('.message').textContent = "You lost the game";
+          document.querySelector('.score').textContent = 0;
+        }
+        
+    }
+    else if(guess < secret_number){
+        if(score > 1){
+            document.querySelector('.message').textContent = "Too Low";
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+          document.querySelector('.message').textContent = "You lost the game";
+          document.querySelector('.score').textContent = 0;
+        }
     }
 }
 
